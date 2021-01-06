c981a57a2f9f3013b552aa6f5f5da6050ef8249b
RatBoyJim <ratboyjim@gmail.com>
2021-01-06 17:27:04 +0000

    library tests started

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> bookList;
+
+    public Library(){
+        this.bookList = new ArrayList<>();
+    }
+
+    public int bookCount() {
+        return this.bookList.size();
+    }
 }
